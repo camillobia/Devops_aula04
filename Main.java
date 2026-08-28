db3f4c2ff0143027311e7e942edbe03d17293b16
camillobia <beatriz.lombardo2002@gmail.com>
2026-08-28 19:46:34 -0300

    Prática 06

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
-System.out.println("===SISTEMA DE CURSOS===");
->>>>>>> Stashed changes
 
-Curso curso1 = new Curso("Desenvolvimento Java","Carlos Silva",40,800.00);
+        System.out.println("=== SISTEMA DE CURSOS ===");
 
-Curso curso2 = new Curso("Desenvolvimento Web","Ana Souza",60,1200.00);
+        Curso curso1 = new Curso(
+            "Desenvolvimento Java",
+            "Carlos Silva",
+            40,
+            800.00
+        );
 
-Curso curso3 = new Curso("Banco de Dados","Marcos Oliveira",30,600.00); 
+        Curso curso2 = new Curso(
+            "Desenvolvimento Web",
+            "Ana Souza",
+            60,
+            1200.00
+        );
+
+        Curso curso3 = new Curso(
+            "Banco de Dados",
+            "Marcos Oliveira",
+            30,
+            600.00
+        );
 
         curso1.exibirInformacoes();
         System.out.println();
@@ -20,4 +33,4 @@ Curso curso3 = new Curso("Banco de Dados","Marcos Oliveira",30,600.00);
 
         curso3.exibirInformacoes();
     }
-}   
+}
